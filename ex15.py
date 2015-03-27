4ea51a1771f27c2e54d466214ae754cafc32f4d9
Alex Miles Younger <alex@alexmilesyounger.com>
2015-03-27 19:35:49 -0400

    Comment ex 15

--- ex15.py
+++ ex15.py
@@ -1,15 +1,25 @@
+# Import the sys module and the argv function
 from sys import argv
 
+# Unpack argv, script is required, 
+# then one argument for each variable we want
 script, filename = argv
 
+# Define text as the contents of filename
 txt = open(filename)
 
+# Print quote + variable
 print "Here's your file %r:" % filename
+
+# Print contents read() of txt variable
 print txt.read()
 
+# Accept new input for file_again var
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# Define txt_again as the contents of file_again
 txt_again = open(file_again)
 
+# Print contents read() of txt_again var
 print txt_again.read()
