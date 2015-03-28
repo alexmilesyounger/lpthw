6c5589c71a552a4bb2c192e6ab924706456e64ef
Alex Miles Younger <alex@alexmilesyounger.com>
2015-03-28 13:48:27 -0400

    Add shorthand notation for incrimenting variable count

--- ex20.py
+++ ex20.py
@@ -33,26 +33,11 @@ rewind(current_file)
 # Print three lines using the print_a_line function
 print "Let's print three lines:\n"
 
-current_line = 1		# Setting current_line = 1 sets the start
-						# point for the line counter
-
+current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1		# Adding one to current_line
-						# increments the current_line count up by
-						# one, increaseing the line count to the
-						# left of the line displayed
-						# This counter only works if you rewind
-						# the file to zero before you run it
-						# it does not autmatically count the line
-						# number from the beginning
-
+current_line += 1
 print_a_line(current_line, current_file)
-						# print_a_line automatically incriments 
-						# so that it prints the next line without
-						# repeating lines. To repeat a line on 
-						# purpose I think you'd need to force the 
-						# file to rewind 1 line
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
